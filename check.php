782821b674d5c70fdb4029c4c7360f2881b58d8c
Wang Xing <wangstar1031@hotmail.com>
2019-02-27 04:31:48 +0800

    Removed comments.

--- check.php
+++ check.php
@@ -1,24 +1,17 @@
 <?php
     function domain_name($domain_name)
     {
-        //FILTER_VALIDATE_URL checks length but..why not? so we dont move forward with more expensive operations
         $domain_len = strlen($domain_name);
         if ($domain_len < 3 OR $domain_len > 253)
             return FALSE;
-        //getting rid of HTTP/S just in case was passed.
         if(stripos($domain_name, 'http://') === 0)
             $domain_name = substr($domain_name, 7); 
         elseif(stripos($domain_name, 'https://') === 0)
             $domain_name = substr($domain_name, 8);
-        
-        //we dont need the www either                 
         if(stripos($domain_name, 'www.') === 0)
             $domain_name = substr($domain_name, 4); 
-        //Checking for a '.' at least, not in the beginning nor end, since http://.abcd. is reported valid
         if(strpos($domain_name, '.') === FALSE OR $domain_name[strlen($domain_name)-1]=='.' OR $domain_name[0]=='.')
             return FALSE;
-                 
-        //now we use the FILTER_VALIDATE_URL, concatenating http so we can use it, and return BOOL
         return (filter_var ('http://' . $domain_name, FILTER_VALIDATE_URL)===FALSE)? FALSE:TRUE;
     }
     $contents = file_get_contents("info.html");
